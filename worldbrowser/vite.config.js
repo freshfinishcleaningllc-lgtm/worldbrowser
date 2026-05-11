1026e5e2e5eb6c93b3d2a565177d7b5f2e4fba9f
freshfinishcleaningllc-lgtm <freshfinishcleaningllc@gmail.com>
2026-05-11 18:56:18 -0500

    Update vite.config.js

--- worldbrowser/vite.config.js
+++ worldbrowser/vite.config.js
@@ -19,8 +19,11 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+  { src: '/favicon.svg', sizes: '192x192', type: 'image/svg+xml' },
+  { src: '/favicon.svg', sizes: '512x512', type: 'image/svg+xml', purpose: 'any maskable' }
+]
+          
+          
         ]
       },
       workbox: {
